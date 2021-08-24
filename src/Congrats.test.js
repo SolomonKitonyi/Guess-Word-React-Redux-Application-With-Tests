30d8c8e01934ce8e275466194a1bf29e5b743985
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-24 09:57:00 +0300

    import test utils findtestAttribute

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,8 +2,13 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
+import { findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
 
 test("renders without error", () => {});
 test("renders no text when `success prop` is false", () => {});
