ac96122b5056cbfd5b0b644d3d08cfeb2ff42a9a
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-24 09:54:28 +0300

    write initial tests

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -4,3 +4,7 @@ import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 import Congrats from "./Congrats";
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+test("renders no text when `success prop` is false", () => {});
+test("renders non-empty congrats message when success prop is true", () => {});
