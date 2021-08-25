b52ded8c23317b8e33957dd0110559f890ba4f62
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-25 09:54:16 +0300

    add checkProps util

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,15 @@
+import PropTypes from "prop-types";
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, confirmingProps) => {
+  const propError = PropTypes.checkPropTypes(
+    component.propTypes,
+    confirmingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
