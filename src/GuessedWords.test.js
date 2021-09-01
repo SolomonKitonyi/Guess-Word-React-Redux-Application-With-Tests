a6f13ec6b72da83150a74b0670b0a2d7fd379f6f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-01 10:30:06 +0300

    add changes

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -4,3 +4,16 @@ import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 
 import GuessedWords from "./Guessedwords";
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("Does not throw warnings given expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
