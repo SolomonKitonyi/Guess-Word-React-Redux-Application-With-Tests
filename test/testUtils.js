2f036b111b10d4b66c31082c77484380a837c16d
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-10-04 10:02:18 +0300

    add storeFactory function

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,15 +1,22 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+export const storeFactory = (initialState) => {
+	return createStore(rootReducer, initialState);
+};
 
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
+	return wrapper.find(`[data-test="${val}"]`);
 };
 
 export const checkProps = (component, confirmingProps) => {
-  const propError = PropTypes.checkPropTypes(
-    component.propTypes,
-    confirmingProps,
-    "prop",
-    component.name
-  );
-  expect(propError).toBeUndefined();
+	const propError = PropTypes.checkPropTypes(
+		component.propTypes,
+		confirmingProps,
+		'prop',
+		component.name
+	);
+	expect(propError).toBeUndefined();
 };
