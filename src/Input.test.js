b104b67ccad971a3b79d9131775794e81cb32b78
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-10-19 10:25:17 +0300

    add tests  for guessWord

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
+import secretWord from './reducers/secretWordReducer';
 
 const setup = (initialState = {}) => {
 	const store = storeFactory(initialState);
@@ -66,5 +67,25 @@ describe('redux props', () => {
 		const wrapper = setup();
 		const guessedWordProp = wrapper.instance().props.guessWord;
 		expect(guessedWordProp).toBeInstanceOf(Function);
+	});
+});
+
+describe('`guessWord` action creator call', () => {
+	test('calls `guessWord` when button is clicked', () => {
+		const guessWordMock = jest.fn();
+		const props = {
+			guessWord: guessWordMock,
+		};
+
+		//set up wrapper Input with guessWordMock as guessWord prop
+		const wrapper = shallow(<UnconnectedInput {...props} />);
+
+		//simulate click
+		const submitButton = findByTestAttr(wrapper, 'submit-button');
+		submitButton.simulate('click');
+
+		//check to see if mock ran
+		const guessWordCallCount = guessWordMock.mock.calls.length;
+		expect(guessWordCallCount).toBe(1);
 	});
 });
